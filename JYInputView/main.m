31b97e7b785380b096949a029ef51abab6a21d98
金悦悦 <921386013@qq.com>
2017-09-27 14:16:20 +0800

    Initial Commit

---
+++ JYInputView/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  JYInputView
+//
+//  Created by 伟运体育 on 2017/9/27.
+//  Copyright © 2017年 伟运体育. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
